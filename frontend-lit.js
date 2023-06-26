bfb5cb9ccc69fc9fbaf8036663d9a848d76ecedc
Jens Simon <jens.simon80@googlemail.com>
2023-06-26 09:16:15 +0200

    feat: don't remove semicolon in 6.x version

--- frontend-lit.js
+++ frontend-lit.js
@@ -1,1 +1,1 @@
-module.exports = require('./lib/frontend/lit')
+module.exports = require('./lib/frontend/lit');
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const commonConfig = require('./lib/common')
+const commonConfig = require('./lib/common');
 
 module.exports = commonConfig({
   extends: [
@@ -14,4 +14,4 @@ module.exports = commonConfig({
       },
     ],
   },
-})
+});
